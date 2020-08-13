fa532b6e22d0b45736713d2dd94e0fe25c4a77f2
Alexey <lasrtg1@googlemail.com>
2020-08-13 22:46:38 +0300

    added Image

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
-MODEL_NAME = 'distilbert-base-uncased' # pretrained model from Transformers
-LOG_DIR = "./logdir_amazon"    # for training logs and tensorboard visualizations
-NUM_EPOCHS = 1                         # smth around 2-6 epochs is typically fine when finetuning transformers
-BATCH_SIZE = 32                       # depends on your available GPU memory (in combination with max seq length)
-MAX_SEQ_LENGTH = 512                   # depends on your available GPU memory (in combination with batch size)
-LEARN_RATE = 3e-5                      # learning rate is typically ~1e-5 for transformers
-ACCUM_STEPS = 3                        # one optimization step for that many backward passes
-SEED = 42                              # random seed for reproducibility
-fp16_params = dict(opt_level="O1")
+MODEL_NAME = 'distilbert-base-uncased'
+LOG_DIR = "./amazon"
+NUM_EPOCHS = 2
+BATCH_SIZE = 32
+MAX_SEQ_LENGTH = 512
+LEARN_RATE = 5e-5
+WEIGHT_DECAY = 1e-3
+ACCUM_STEPS = 3
+SEED = 42
+FP_16 = dict(opt_level="O1")
